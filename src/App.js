aa56168d81f33aa518bdd9e85a1bd7fc772047a9
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-10-10 19:01:46 -0600

    Got back to day 1 mvp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+
+  const [nasaImg, setImg] = useState(0);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const nasaData = await axios.get('https://api.nasa.gov/planetary/apod?api_key=ZzLgOgfo6E53p1V82P8OUWw85SVcFBfyeXxFAaTe')
+        console.log(nasaData);
+        setImg(nasaData.data);
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData()
+  }, [])
+  console.log(nasaImg);
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+      <h1>NASA picture of the day!</h1>
+      <h2>{nasaImg.title}</h2>
+      <h2>{nasaImg.date}</h2>
+      <img src={`${nasaImg.url}`} />
+      <p class="expl-1">{nasaImg.explanation}</p>
       </p>
     </div>
   );

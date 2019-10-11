c93d613e91b10485c1fe07f5553118d4a880eb94
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-10-10 20:35:53 -0600

    mvp day 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
+import styled from 'styled-components';
 
-function App() {
+const Container = styled.div`
+    text-align: center;
+    max-width: 90%;
+    width: 80%;
+    margin: 0 auto;
+    background-color: white;
+    border-radius: 1rem;
+    padding: 0.5rem;
+    box-shadow: 10px 50px 50px rgba(0, 0, 0, 0.5);
+`;
+
+const Test = styled.button`
+  background: #0B3D91;
+  border-radius: 10px;
+  border: 1px solid #FC3D21;
+  color: white;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  height: 45px;
+  width: 20%;
+  font-size: 1.25rem;
+`
+
+const TopPic = styled.img`
+margin: 0 35%;
+width: 200px;
+height: 200px;
+`;
+
+const App = () => {
 
   const [nasaImg, setImg] = useState(0);
 
@@ -22,16 +52,21 @@ function App() {
   console.log(nasaImg);
 
   return (
-    <div className="App">
+    <Container>
       <p>
+      <TopPic img src="http://choosewashingtonstate.com/wp-content/uploads/2017/04/NASALogo-1.jpg"></TopPic>
+      <Test>Home</Test> <Test>About Us</Test> <Test>Contact</Test>
       <h1>NASA picture of the day!</h1>
       <h2>{nasaImg.title}</h2>
       <h2>{nasaImg.date}</h2>
-      <img src={`${nasaImg.url}`} />
+      <img src={`${nasaImg.url}`}/>
       <p class="expl-1">{nasaImg.explanation}</p>
+      <Test>Home</Test> <Test>About Us</Test> <Test>Contact</Test>
       </p>
-    </div>
+    </Container>
   );
 }
+
+
 
 export default App;
